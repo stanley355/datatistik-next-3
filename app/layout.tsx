a9c8ad70b92f79d418de3de141d2323dbff005af
Stanley Winata <winatastanley355@gmail.com>
2026-05-01 13:14:36 +0700

    feat: Update css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const jetbrains = JetBrains_Mono({ variable: "--font-mono" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +20,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full antialiased font-sans", inter.variable)}
+      className={cn(
+        "h-full antialiased font-mono",
+        inter.variable,
+        jetbrains.variable,
+      )}
       suppressHydrationWarning
     >
       <body>
