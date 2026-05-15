c10cef2a65c39eb0180dd2ba0af438ad9916dd8a
Stanley Winata <winatastanley355@gmail.com>
2026-05-15 12:27:50 +0700

    Revert "feat: Cookie"
    This reverts commit ce28c174c37674d03b3e1b8532704056748b82a8.

--- app/(public)/account/page.tsx
+++ app/(public)/account/page.tsx
@@ -1,12 +1,9 @@
 import { Account } from "./_components";
-import { AuthPage } from "@/components/custom-ui/auth-page";
 
 export default function Page() {
   return (
-    <AuthPage>
-      <div className="container mx-auto mt-16 p-4 min-h-screen">
-        <Account />
-      </div>
-    </AuthPage>
+    <div className="container mx-auto mt-16 p-4 min-h-screen">
+      <Account />
+    </div>
   );
 }
